1f785393b35c6ff4afd15cfa19f4d0720ee997a6
Ian <20307110123@fudan.edu.cn>
2023-08-23 11:35:40 +0000

    flash_attn_for prunedllama

--- src/lmflow/utils/flash_attention/llama_flash_attention.py
+++ src/lmflow/utils/flash_attention/llama_flash_attention.py
@@ -16,6 +16,7 @@ except:
 
 from flash_attn.bert_padding import unpad_input, pad_input
 
+import LLMPruner
 
 def forward(
     self,
@@ -117,3 +118,10 @@ def replace_llama_attn_with_flash_attn():
         _prepare_decoder_attention_mask
     )
     transformers.models.llama.modeling_llama.LlamaAttention.forward = forward
+    LLMPruner.models.hf_llama.modeling_llama.LlamaModel._prepare_decoder_attention_mask = (
+        _prepare_decoder_attention_mask
+    )
+    LLMPruner.models.hf_llama.modeling_llama.PrunedLlamaModel._prepare_decoder_attention_mask = (
+        _prepare_decoder_attention_mask
+    )
+    LLMPruner.models.hf_llama.modeling_llama.LlamaAttention.forward = forward
