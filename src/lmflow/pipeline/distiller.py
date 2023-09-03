0d98a94933549f5ff5b43678c898a352e2063af4
ianz <20307110123@fudan.edu.cn>
2023-09-04 00:57:29 +0800

    fix bug when computing kd

--- src/lmflow/pipeline/distiller.py
+++ src/lmflow/pipeline/distiller.py
@@ -71,7 +71,7 @@ class Trainer_with_distillation(Trainer):
             teacher_logits = teacher_outputs['logits']
 
         #compute kl loss of soft lables
-        student_log_sm = torch.nn.functional.softmax(student_logits / self.kl_t, dim=-1)
+        student_log_sm = torch.nn.functional.log_softmax(student_logits / self.kl_t, dim=-1)
         teacher_log_sm = torch.nn.functional.softmax(teacher_logits / self.kl_t, dim=-1)
         kl_loss = torch.nn.functional.kl_div(input=student_log_sm, target=teacher_log_sm) * (self.kl_t ** 2) 
 
@@ -79,10 +79,9 @@ class Trainer_with_distillation(Trainer):
         teacher_hidden_states = torch.stack(teacher_hidden_states)
         #compute mse loss of hidden states
         mse_loss = torch.nn.functional.mse_loss(input = student_hidden_states, target = teacher_hidden_states)
-        print(f'hard label loss: {loss}, soft label loss: {kl_loss}, MSE loss: {mse_loss}')
-        loss = loss + kl_loss * self.kl_w + mse_loss * self.mse_w
-
-        return (loss, outputs) if return_outputs else loss
+        weighted_loss = loss + kl_loss * self.kl_w + mse_loss * self.mse_w
+        print(f'hard label loss: {loss}, soft label loss: {kl_loss}, MSE loss: {mse_loss}, weighted sum: {weighted_loss}')
+        return (weighted_loss, outputs) if return_outputs else weighted_loss
 
 
 class Distiller(BaseTuner):
